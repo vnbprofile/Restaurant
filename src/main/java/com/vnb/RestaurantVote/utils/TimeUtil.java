4135fcb94a4ffd1171eabff2b3bcf1fac0a1326f
Vlad <vladislav.vnb2020@gmail>
2020-10-26 01:57:48 +0300

    update

--- src/main/java/com/vnb/RestaurantVote/utils/TimeUtil.java
+++ src/main/java/com/vnb/RestaurantVote/utils/TimeUtil.java
@@ -10,7 +10,7 @@ public class TimeUtil {
     public static int limitHourForVote = 11;
 
     private TimeUtil() {
-    }
+        }
 
     public static boolean canVote(Restaurant restaurant) {
         if (restaurant.getCreatedDate().isEqual(LocalDate.now())) {
